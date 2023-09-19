bd27b3b3e1af3da1b9e7eb4bde0dd5c09b15ed92
jasmin <jasminchakma895@gmail.com>
2023-09-19 18:24:27 +0600

    use useNavigate

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
+import './Navbar.css'
 const Navbar = () => {
     return (
-        <div className="">
+        <div className="nav">
             <h1 className="my-5">Header</h1>
-            <Link to= '/'> <button className="">Home</button></Link>
-            <Link to= '/about'> <button className="ms-3">About</button></Link>
-            <Link to= '/contact'><button className="ms-3">Contact</button></Link>
-            <Link to= '/users'><button className="ms-3">Users</button></Link>
+            <NavLink to= '/'> <button className="">Home</button></NavLink>
+            <NavLink to= '/about'> <button className="ms-3">About</button></NavLink>
+            <NavLink to= '/contact'><button className="ms-3">Contact</button></NavLink>
+            <NavLink to= '/users'><button className="ms-3">Users</button></NavLink>
 
         </div>
     );
